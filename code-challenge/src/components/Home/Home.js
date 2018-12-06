2281dfa8b649155cf3b60a99580bcfa20c6728bd
AndyRoush <andyclan@gmail.com>
2018-12-05 23:02:34 -0800

    added modal and ready to invest button. Modal working properly. Button placement seems good

--- code-challenge/src/components/Home/Home.js
+++ code-challenge/src/components/Home/Home.js
@@ -1,10 +1,36 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Row, Col, Image, Button, Glyphicon } from "react-bootstrap";
+import {
+  Row,
+  Col,
+  Image,
+  Button,
+  Glyphicon,
+  Modal,
+} from "react-bootstrap";
 import "./Home.css";
 import placeholder from "../../assets/placeholder.png";
 
 class Home extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+
+    this.state = {
+      show: false
+    };
+  }
+
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
+  }
+
   render() {
     return (
       <div className="main-div">
@@ -21,7 +47,12 @@ class Home extends Component {
             </p>
           </Col>
           <Col xs={12} md={4}>
-            <Image className="videoimg" src={placeholder} responsive />
+            <Image
+              className="videoimg"
+              src={placeholder}
+              onClick={this.handleShow}
+              responsive
+            />
           </Col>
         </Row>
 
@@ -31,37 +62,66 @@ class Home extends Component {
               <Glyphicon glyph="glyphicon glyphicon-signal" />
             </div>
             <p className="no-margin-p">Asset Type</p>
-            <p><strong>Wireless Infrastructure</strong></p>
+            <p>
+              <strong>Wireless Infrastructure</strong>
+            </p>
           </Col>
           <Col xs={6} md={2} align="center">
             <div className="glyph-wrapper">
               <Glyphicon glyph="glyphicon glyphicon-resize-full" />
             </div>
             <p className="no-margin-p">Offering Size</p>
-            <p><strong>$100,000,000</strong></p>
+            <p>
+              <strong>$100,000,000</strong>
+            </p>
           </Col>
           <Col xs={6} md={2} align="center">
             <div className="glyph-wrapper">
               <Glyphicon glyph="glyphicon glyphicon-piggy-bank" />
             </div>
             <p className="no-margin-p">Minimum Investment</p>
-            <p><strong>$50,000</strong></p>
+            <p>
+              <strong>$50,000</strong>
+            </p>
           </Col>
           <Col xs={6} md={2} align="center">
             <div className="glyph-wrapper">
               <Glyphicon glyph="glyphicon glyphicon-user" />
             </div>
             <p className="no-margin-p">Investor Suitability</p>
-            <p><strong>Accredited Investors Only</strong></p>
+            <p>
+              <strong>Accredited Investors Only</strong>
+            </p>
           </Col>
           <Col xs={6} md={2} align="center">
             <div className="glyph-wrapper">
               <Glyphicon glyph="glyphicon glyphicon-time" />
             </div>
             <p className="no-margin-p">How long?</p>
-            <p><strong>When the clock strikes 12</strong></p>
+            <p>
+              <strong>When the clock strikes 12</strong>
+            </p>
           </Col>
         </Row>
+
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>
+              <Glyphicon glyph="glyphicon glyphicon-search" /> Media Player
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Image className="videoimg" src={placeholder} responsive />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button className="modal-close" onClick={this.handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        <div id="invest-btn">
+          <button class="invest-btn-text"><strong>Ready to Invest? <Glyphicon glyph="glyphicon glyphicon-pencil" /></strong></button>
+        </div>
       </div>
     );
   }
